57b0defed2e0e37d54fb669b182a695352a20078
SVO89 <dime4000@gmail.com>
2022-06-11 13:05:27 -0600

    6/7/2022 - New Work Files - 1305 hrs cst

--- src/test/java/com/cydeo/test/day10_UtilitiesAndWindows/T02_CRMLogin.java
+++ src/test/java/com/cydeo/test/day10_UtilitiesAndWindows/T02_CRMLogin.java
@@ -1,5 +1,7 @@
 package com.cydeo.test.day10_UtilitiesAndWindows;
 
+import java.util.MissingFormatArgumentException;
+
 public class T02_CRMLogin {
 
 }
---
+++ src/test/java/com/cydeo/test/day10_UtilitiesAndWindows/T03_WindowHandle.java
@@ -0,0 +1,11 @@
+package com.cydeo.test.day10_UtilitiesAndWindows;
+
+import com.cydeo.test.base.TestBase;
+import org.testng.annotations.Test;
+
+public class T03_WindowHandle extends TestBase {
+    @Test
+    public void windowHandleTest(){
+
+    }
+}
