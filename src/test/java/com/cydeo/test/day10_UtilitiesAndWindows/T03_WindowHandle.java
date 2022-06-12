e1672348c8b651d503d083cf4f761d34fb038a5e
SVO89 <dime4000@gmail.com>
2022-06-12 14:29:53 -0600

    6/12/2022 - WindowHandle file created

--- src/test/java/com/cydeo/test/day10_UtilitiesAndWindows/T03_WindowHandle.java
+++ src/test/java/com/cydeo/test/day10_UtilitiesAndWindows/T03_WindowHandle.java
@@ -6,11 +6,15 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Set;
+
 public class T03_WindowHandle extends TestBase {
     @Test
     public void windowHandleTest(){
         // 02. Go to : https://practice.cydeo.com/windows
         driver.get("https://practice.cydeo.com/windows");
+        String mainWindow = driver.getWindowHandle();
+        System.out.println(mainWindow);
         // 03. Assert: Title is “Windows”
         String actualTitle = driver.getTitle();
         String expectedTitle = "Windows";
@@ -19,7 +23,17 @@ public class T03_WindowHandle extends TestBase {
         WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
         clickHereLink.click();
         // 05. Switch to new Window.
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+        for (String each : allWindowsHandles){
+            // window handle 1 - main window
+            // window handle 2 - 2nd window
+            driver.switchTo().window(each);
+            System.out.println("driver.getTitle() = " + driver.getTitle());
+        }
         // 06. Assert: Title is “New Window”
+        String expectedTitle2 = "New Window";
+        actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle2, "NEW WINDOW TITLE TEST : FAILED");
     }
 }
 // 01. Open a chrome browser
