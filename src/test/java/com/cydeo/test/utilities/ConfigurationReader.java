7030517e9ede020428980cac89ce4f75066c4cb3
SVO89 <dime4000@gmail.com>
2022-06-16 18:41:11 -0600

    6/16/2022 - Update to ConfigurationReader 1.2

--- src/test/java/com/cydeo/test/utilities/ConfigurationReader.java
+++ src/test/java/com/cydeo/test/utilities/ConfigurationReader.java
@@ -1,5 +1,19 @@
 package com.cydeo.test.utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigurationReader {
 
+    private static Properties properties = new Properties();
+    static{
+        try {
+            FileInputStream file = new FileInputStream("configuration.properties");
+            properties.load(file);
+        } catch (IOException e){
+            System.out.println("File not found in configuration class");
+            e.printStackTrace();
+        }
+    }
 }
