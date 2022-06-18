caea35da9d6686346db65030c68a60ee1c53b62f
SVO89 <dime4000@gmail.com>
2022-06-18 10:03:33 -0600

    6/16/2022 - Added Singleton & Singleton Practice

---
+++ src/test/java/com/cydeo/test/day14_DriverUtilAndJavaFaker/SingletonPractice.java
@@ -0,0 +1,14 @@
+package com.cydeo.test.day14_DriverUtilAndJavaFaker;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractice {
+
+    @Test
+    public void singleton_understanding_test(){
+
+
+
+
+    }
+}
